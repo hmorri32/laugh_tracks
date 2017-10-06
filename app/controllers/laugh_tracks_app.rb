184f80c7eed954378df9726604325b2e93fce296
Hugh Morrison <hmorri32@gmail.com>
2017-10-06 11:00:39 -0600

    write routes in laughtrack controller. adds test routes to learn bout those params

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,19 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/' do
+    "SUH DUDE!!"
+  end
+
+  get '/comedians' do
+    @specials  = Special.all
+    if params[:age]
+      @matches = Comedian.where(age: params[:age])
+    end
+    erb :comedians
+  end
+
+  get '/test/?:tacos/?:stuff?' do
+    # /test/cool/whaever
+    "Hello #{params[:tacos]}, #{params[:stuff]}"
+  end
 end
